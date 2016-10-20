bfe914a48bc3159aa09633f1268557f2ffe76884
Yu Sun <volunteerbj@sina.com>
2016-10-20 13:43:55 +0800

    Update Transfer_func.sql

--- Transfer_func.sql
+++ Transfer_func.sql
@@ -1,7 +1,33 @@
-create or replace functon transfer (
-      i_payer text,
-      i_recipient text,
-      i_amount  numeric(15,2))
+create or replace funciton transfer(
+		i_payer		text,
+		i_recipient	text,
+		i_amount	numeric(15,2)
+)
 returns text
-as 
+as
 $$
+declare	payer_bal	numeric;
+begin
+	select	balance		into payer_bal
+		from accounts
+	where	owner = i_payer for update;
+	if not found then
+		return 'Payer account not found';
+	end if;
+	if payer_bal < i_amount then
+		return 'Not enough funds'
+	end if;
+
+	update	accounts
+		set balance = balance + i_amount
+		where owner = i_recipient;
+	if not found then
+		return 'Recipient does not exist'
+	end if;
+
+	update accounts
+		set balance = balance - i_amount
+		where	owner = i_payer;
+		return 'OK';
+end;
+$$	language	plpgsql;
